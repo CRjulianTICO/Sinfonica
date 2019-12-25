b8ebb569280e5832b854f18ac33f1a3372185880
Julian <crjuliantico>
2019-12-24 20:09:11 -0600

    Ultimas entities

---
+++ Sinfonica.Web/Areas/Admin/Data/Entities/Estudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sinfonica.Web.Areas.Admin.Data.Entities
+{
+    public class Estudiante : IEntity, IPersona
+    {
+        public int Id { get ; set ; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public int Telefono { get; set; }
+        public string Correo { get; set; }
+        public bool Estado { get; set; }
+
+        public String NombreCompleto { get { return $"{this.Nombre} {this.PrimerApellido} {this.SegundoApellido}"; } }
+
+        public String Carnet { get; set; }                          
+
+    }
+}
---
+++ Sinfonica.Web/Areas/Admin/Data/Entities/Noticia.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sinfonica.Web.Areas.Admin.Data.Entities
+{
+    public class Noticia : IEntity
+    {
+        public int Id { get; set; }
+
+        public String Titulo { get; set; }
+
+        public String Encabezado { get; set; }
+
+        public String Informacion { get; set; }
+
+        public String Descripcion { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public Boolean Estado { get; set; }
+
+        public string MonthName
+        {
+            get
+            {
+                String mes = Fecha.ToString("MMMM", CultureInfo.CreateSpecificCulture("es"));
+
+                mes = char.ToUpper(mes[0]) + mes.Substring(1);
+                mes = mes.TrimEnd('.');
+
+                return mes;
+            }
+        }
+
+        public string MonthShortName
+        {
+            get
+            {
+                String mes = Fecha.ToString("MMM", CultureInfo.CreateSpecificCulture("es"));
+
+                mes = char.ToUpper(mes[0]) + mes.Substring(1);
+                mes = mes.TrimEnd('.');
+
+                return mes;
+            }
+        }
+
+        [Display(Name = "Image")]
+        public string ImageUrl
+        {
+            get;
+            set;
+        }
+
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.ImageUrl))
+                {
+                    return null;
+                }
+                return $"https://xaesw.azurewebsites.net{this.ImageUrl.Substring(1)}";
+            }
+        }
+
+    }
+}
+
