8a8dbc43175afd9fb31eeed21214681b2d891728
Oscar SL <oscarsoto040797@gmail.com>
2020-02-10 20:17:10 -0600

    Logo

---
+++ Sinfonica.Web/Migrations/20200204221033_CambioImageConjuntos.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sinfonica.Web.Migrations
+{
+    public partial class CambioImageConjuntos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
